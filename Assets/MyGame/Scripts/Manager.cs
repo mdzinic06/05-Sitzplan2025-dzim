9e2aa5977028795da9b7a768b011c7b3f3cdb211
mdzinic06 <mirza.dzinic06@htl-salzburg.ac.at>
2025-09-27 21:58:38 +0200

    changes in assets and adding a new clickscript

---
+++ Assets/MyGame/Scripts/Manager.cs
@@ -0,0 +1,10 @@
+using Unity.VisualScripting;
+using Unity.VisualScripting.Dependencies.Sqlite;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.EventSystems;
+
+public class Manager : MonoBehaviour
+{
+
+}
