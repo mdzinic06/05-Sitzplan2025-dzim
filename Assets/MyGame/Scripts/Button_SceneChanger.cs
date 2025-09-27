c70c020b3c03e67545b9498edb0307b5e5e411cc
mdzinic06 <mirza.dzinic06@htl-salzburg.ac.at>
2025-09-27 17:35:18 +0200

    adding new Text to the plane and rewriting the text in the script

--- Assets/MyGame/Scripts/Button_SceneChanger.cs
+++ Assets/MyGame/Scripts/Button_SceneChanger.cs
@@ -5,10 +5,12 @@ using UnityEngine.SceneManagement;
 public class Button_SceneChanger : MonoBehaviour
 {
     public TMP_Text text_;
+    public TMP_Text text_for_screen;
 
     private void Awake()
     {
         text_.text = "Start";
+        text_for_screen.text = "Klick Start to start in the Classroom 4BHITS!";
     }
 
     public void ChangingScene_()
