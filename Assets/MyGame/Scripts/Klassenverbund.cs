8ed2ded2c8062884636a8fac0480845f6e9f64ea
mdzinic06 <mirza.dzinic06@htl-salzburg.ac.at>
2025-09-28 19:34:46 +0200

    adding another input and textfield

--- Assets/MyGame/Scripts/Klassenverbund.cs
+++ Assets/MyGame/Scripts/Klassenverbund.cs
@@ -5,11 +5,18 @@ public class Klassenverbund : MonoBehaviour
 {
 
     public TMP_Text text_input;
+    public TMP_Text text_text_field;
+
+    public TMP_Text text_input_table;
+    public TMP_Text text_output_text_field_table;
     Student student = new Student();
 
     void Start()
     {
         text_input.text = "Enter ID";
+        text_text_field.text = " ";
+        text_input_table.text = "Enter table number!";
+        text_output_text_field_table.text = " ";
     }
 
     // Update is called once per frame
