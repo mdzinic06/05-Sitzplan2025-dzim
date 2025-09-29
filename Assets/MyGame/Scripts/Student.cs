fedb00b07816626a3327c3f3b0afab9b8eceab7b
mdzinic06 <mirza.dzinic06@htl-salzburg.ac.at>
2025-09-29 10:52:36 +0200

    changes in script

--- Assets/MyGame/Scripts/Student.cs
+++ Assets/MyGame/Scripts/Student.cs
@@ -1,31 +1,24 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Student : MonoBehaviour
 {
-    [SerializeField] private string name_student;
-    [SerializeField] private string age_student;
-    [SerializeField] private string anwesenheit_student;
-    [SerializeField] private string id_student;
-
-    private Mensch mensch;  // hier speichern wir die Daten
+    [SerializeField] private List<Mensch> schuelerListe = new List<Mensch>();
 
     void Start()
     {
-        mensch = new Mensch();
-        mensch.set_name(name_student);
-        mensch.set_age(age_student);
-        mensch.set_anwesenheit(anwesenheit_student);
-        mensch.set_id(id_student);
-
-        // Ausgabe in der Konsole
-        Debug.Log($"Schüler: Name={mensch.get_name()}, " +
-                  $"Alter={mensch.get_age()}, " +
-                  $"Anwesenheit={mensch.get_anwesenheit()}, " +
-                  $"ID={mensch.get_id()}");
-    }
-
-    public Mensch GetMensch()
-    {
-        return mensch;
+        // Alle Schüler ausgeben
+        for (int i = 0; i < schuelerListe.Count; i++)
+        {
+            Mensch m = schuelerListe[i];
+            if (m != null)
+            {
+                Debug.Log($"Schüler {i + 1}: " +
+                          $"Name={m.get_name()}, " +
+                          $"Alter={m.get_age()}, " +
+                          $"Anwesenheit={m.get_anwesenheit()}, " +
+                          $"ID={m.get_id()}");
+            }
+        }
     }
 }
